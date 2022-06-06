950dd02967c54d1197c91f2cf145ed580e0a414e
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-06-06 18:57:04 +0530

    Added Jump Functionality

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.up* 10, ForceMode.Impulse);
+        }
+    }
+}
