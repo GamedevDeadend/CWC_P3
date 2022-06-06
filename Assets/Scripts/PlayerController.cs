9ad31a2a8a7ce216705da2c066f5c5597ba0c7af
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-06-06 19:42:51 +0530

    Added Obstacle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public float ForceApplied;
+    public float GravityMultiplier;
+
+    public bool OnGround;
     // Start is called before the first frame update
     void Start()
     {
+        OnGround = true;
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= GravityMultiplier;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space ) && OnGround)
         {
-            rb = GetComponent<Rigidbody>();
-            rb.AddForce(Vector3.up* 10, ForceMode.Impulse);
+            rb.AddForce(Vector3.up* ForceApplied, ForceMode.Impulse);
+            OnGround = false;
         }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        OnGround = true;
+    } 
 }
